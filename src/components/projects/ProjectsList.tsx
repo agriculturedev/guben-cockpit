90c710d7d0283454761bd9bda3a942fade034d20
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-26 13:26:21 +0200

    just formatting and import optimization

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -1,19 +1,17 @@
-import {ProjectListResponseDataItem} from "@/endpoints/gubenProdSchemas";
-import {ProjectCard} from "@/components/projects/ProjectCard";
+import { ProjectListResponseDataItem } from "@/endpoints/gubenProdSchemas";
 import { ProjectCardWithDialog } from "@/components/projects/ProjectCardWithDialog";
 
-
 interface ProjectsListProps {
-    projects?: ProjectListResponseDataItem[];
+  projects?: ProjectListResponseDataItem[];
 }
 
-export const ProjectsList = ({projects}: ProjectsListProps) => {
-    return (
-      <div className={"columns-3"}>
-          {projects &&
-            projects.map((project, index) =>
-              <ProjectCardWithDialog key={index} project={project}/>)
-          }
-      </div>
-    )
+export const ProjectsList = ({ projects }: ProjectsListProps) => {
+  return (
+    <div className={"columns-3"}>
+      {projects &&
+        projects.map((project, index) =>
+          <ProjectCardWithDialog key={index} project={project}/>)
+      }
+    </div>
+  )
 }
