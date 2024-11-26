64ed73a532a5c7c9d090cd3783812885c46415b7
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-11-26 13:26:00 +0100

    run on startup?

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -1,3 +1,5 @@
+import cronJobs from "./guben-cockpit-plugins/cron-jobs";
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -14,5 +16,8 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    // Manually trigger the cron jobs on startup
+    cronJobs.myJob.task({ strapi });
+  },
 };
