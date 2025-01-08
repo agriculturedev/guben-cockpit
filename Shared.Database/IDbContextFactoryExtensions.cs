aee29f7c0ac3f8dc980b5f266a03cdbf5e16345f
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-01-08 21:55:55 +0100

    use transaction extension

--- Shared.Database/IDbContextFactoryExtensions.cs
+++ Shared.Database/IDbContextFactoryExtensions.cs
@@ -15,13 +15,34 @@ public static class IDbContextFactoryExtensions
     try
     {
       var result = await task;
-      await dbContext.SaveChangesAsync().ConfigureAwait(false);;
+      await dbContext.SaveChangesAsync().ConfigureAwait(false);
       await tx.CommitAsync().ConfigureAwait(false);
       return result;
     }
     catch
     {
       await tx.RollbackAsync().ConfigureAwait(false);
+      throw;
+    }
+  }
+
+  public static async Task UseTransaction<TDbContext>(this Task task, ICustomDbContextFactory<TDbContext> dbContextFactory) where TDbContext : DbContext
+  {
+    await using var dbContext = dbContextFactory.CreateNew();
+
+    await using var tx = await dbContext.Database
+      .BeginTransactionAsync();
+
+    try
+    {
+      await task;
+      await dbContext.SaveChangesAsync();
+      await tx.CommitAsync();
+      Console.WriteLine("commit transaction");
+    }
+    catch
+    {
+      await tx.RollbackAsync();
       throw;
     }
   }
@@ -35,7 +56,7 @@ public static class IDbContextFactoryExtensions
     try
     {
       var result = await task;
-      await dbContext.SaveChangesAsync().ConfigureAwait(false);;
+      await dbContext.SaveChangesAsync().ConfigureAwait(false);
       await tx.CommitAsync().ConfigureAwait(false);
       return result;
     }
