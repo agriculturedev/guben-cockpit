8ee03a8b54e84fd0de9936d8a7caff357c563cf4
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-01-25 17:31:16 +0100

    update config

--- Api/Configuration.cs
+++ Api/Configuration.cs
@@ -3,19 +3,12 @@
 public class Configuration
 {
   public ConnectionStrings ConnectionStrings { get; set; } = null!;
-  public KeycloakConfiguration Keycloak { get; set; } = null!;
   public FrontendConfiguration Frontend { get; set; } = null!;
 }
 
 public class ConnectionStrings
 {
   public string DefaultConnection { get; set; } = null!;
-}
-
-public class KeycloakConfiguration
-{
-  public string Authority { get; set; } = null!;
-  public string Audience { get; set; } = null!;
 }
 
 public class FrontendConfiguration
