60c1bebb3df95bd8a57811e0fc1f77001f896278
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-01-28 21:53:39 +0100

    migration + repo

---
+++ Database/Repositories/DashboardRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.DashboardTab;
+using Domain.DashboardTab.repository;
+using Shared.Database;
+
+namespace Database.Repositories;
+
+public class DashboardRepository
+  : EntityFrameworkRepository<DashboardTab, Guid, GubenDbContext>, IDashboardRepository
+{
+  public DashboardRepository(ICustomDbContextFactory<GubenDbContext> dbContextFactory)
+    : base(dbContextFactory)
+  {
+  }
+
+}
