ec7afa749d70378ee77631ee35ce52040909afe6
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-02-04 18:44:35 +0100

    fix component

--- frontend/src/components/dashboard/cards/EditDashboardCards.tsx
+++ frontend/src/components/dashboard/cards/EditDashboardCards.tsx
@@ -4,7 +4,7 @@ import { EditDashboardCardButton } from "@/components/dashboard/cards/editDashbo
 import { DashboardTabResponse } from "@/endpoints/gubenSchemas";
 import { useTranslation } from "react-i18next";
 import { InfoCard } from "@/components/home/InfoCard/InfoCard";
-import { FloatingCardButtons2 } from "@/components/ui/floatingCardButtons";
+import { FloatingCardButtons } from "@/components/ui/floatingCardButtons";
 
 interface Props {
   tab: DashboardTabResponse;
@@ -24,16 +24,16 @@ export const EditDashboardCards = ({tab, refetch}: Props) => {
 
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 pr-4">
         {sortedCards?.map((card, index) => (
-          <FloatingCardButtons2 key={index}>
+          <FloatingCardButtons key={index}>
 
             <InfoCard card={card}/>
 
-            <FloatingCardButtons2.Buttons>
+            <FloatingCardButtons.Buttons>
               <EditDashboardCardButton card={card} dashboardTabId={tab.id} refetch={refetch}/>
               <DeleteDashboardCardButton cardId={card.id} dashboardTabId={tab.id} refetch={refetch}/>
-            </FloatingCardButtons2.Buttons>
+            </FloatingCardButtons.Buttons>
 
-          </FloatingCardButtons2>
+          </FloatingCardButtons>
         ))}
       </div>
     </div>
--- frontend/src/components/ui/floatingCardButtons.tsx
+++ frontend/src/components/ui/floatingCardButtons.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-export const FloatingCardButtons2 = ({children}: PropsWithChildren) => {
+export const FloatingCardButtons = ({children}: PropsWithChildren) => {
   return (
     <div className="flex relative">
       {children}
@@ -8,7 +8,7 @@ export const FloatingCardButtons2 = ({children}: PropsWithChildren) => {
   )
 }
 
-export const FloatingCardButtons2.Buttons = ({children}: PropsWithChildren) => {
+FloatingCardButtons.Buttons = ({children}: PropsWithChildren) => {
   return (
     <div className="flex flex-col gap-2 absolute right-0 top-4 translate-x-[45%]">
       {children}
