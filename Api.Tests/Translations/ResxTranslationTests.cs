1ca59244a915b9512064815855c1f454bdd2485e
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-02-07 16:21:28 +0100

    add extra test for yeeting old keys

--- Api.Tests/Translations/ResxTranslationTests.cs
+++ Api.Tests/Translations/ResxTranslationTests.cs
@@ -1,12 +1,15 @@
 using System.Reflection;
 using System.Xml.Linq;
 using Domain;
+using Shouldly;
 
 namespace Api.Tests.Translations;
 
 public class ResxTranslationTests
 {
-  private const string ResxFolderPath = @"../../../../Api/Infrastructure/Translations"; // Change this to your actual folder containing .resx files
+  private const string
+    ResxFolderPath =
+      @"../../../../Api/Infrastructure/Translations"; // Change this to your actual folder containing .resx files
 
   [Fact]
   public void AllResxFilesShouldHaveSameKeys()
@@ -32,7 +35,8 @@ public class ResxTranslationTests
       var missingKeys = allKeys.Except(keys).ToList();
       if (missingKeys.Any())
       {
-        missingKeysReport.Add($"Language {fileName.Split('.')[1].ToUpper()} is missing keys: {string.Join(", ", missingKeys)}");
+        missingKeysReport.Add(
+          $"Language {fileName.Split('.')[1].ToUpper()} is missing keys: {string.Join(", ", missingKeys)}");
       }
     }
 
@@ -54,11 +58,59 @@ public class ResxTranslationTests
 
       if (missingKeys.Any())
       {
-        missingKeysReport.Add($"Language {Path.GetFileName(file).Split('.')[1].ToUpper()} is missing keys: {string.Join(", ", missingKeys)}");
+        missingKeysReport.Add(
+          $"Language {Path.GetFileName(file).Split('.')[1].ToUpper()} is missing keys: {string.Join(", ", missingKeys)}");
       }
     }
 
     Assert.True(missingKeysReport.Count == 0, string.Join("\n", missingKeysReport));
+  }
+
+  [Fact]
+  public void ShouldNotHaveExtraKeysInTranslationFiles2()
+  {
+    // Get all translation keys from the TranslationKeys class
+    var allTranslationKeys = typeof(TranslationKeys)
+      .GetFields(BindingFlags.Public | BindingFlags.Static)
+      .Select(field => field.GetValue(null).ToString())
+      .ToHashSet();
+
+    // Get all keys from .resx files
+    var resxFiles = Directory.GetFiles(ResxFolderPath, "*.resx");
+
+    var resxKeys = new Dictionary<string, HashSet<string>>();
+
+    foreach (var file in resxFiles)
+    {
+      // Extract language code from the file name (assumes file name includes language code like en.resx, de.resx)
+      var languageCode = Path.GetFileNameWithoutExtension(file).Split('.').Last();
+
+      var doc = XDocument.Load(file);
+      var keys = doc.Descendants("data")
+        .Select(data => data.Attribute("name")?.Value)
+        .Where(name => !string.IsNullOrEmpty(name))
+        .ToHashSet();
+
+      // Add keys to the dictionary grouped by language code
+      resxKeys[languageCode] = keys;
+    }
+
+    // Find extra keys that are in the .resx files but not in the TranslationKeys class
+    var extraKeysByLanguage = resxKeys
+      .Where(kvp => kvp.Value.Any(key => !allTranslationKeys.Contains(key)))
+      .ToDictionary(
+        kvp => kvp.Key,
+        kvp => kvp.Value.Where(key => !allTranslationKeys.Contains(key)).ToList()
+      );
+
+    // Assert that there are no extra keys in any language's .resx files
+    foreach (var language in extraKeysByLanguage)
+    {
+      var languageCode = language.Key;
+      var extraKeys = language.Value;
+      extraKeys.ShouldBeEmpty(
+        $"The following keys are present in the {languageCode}.resx file but not in the TranslationKeys class: {string.Join(", ", extraKeys)}");
+    }
   }
 
   private HashSet<string> GetTranslationKeysFromClass()
