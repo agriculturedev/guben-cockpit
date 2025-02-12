a4905bddfd318dd3ed548ff1edd1a91fa044a300
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2025-02-12 23:00:44 +0100

    show langs differently (#71)

--- frontend/src/utilities/i18n/Languages.ts
+++ frontend/src/utilities/i18n/Languages.ts
@@ -13,3 +13,9 @@ export const getLanguage = (lang: string | null): Language => {
       return Language.de; // Default to 'nl' if the language is not supported
   }
 }
+
+
+export const getLocalizedLanguagename = (language: string) => {
+  // returns the specified languages full name, in that given language so DE will be Deutsch, EN will be Englisch and NL would be Nederlands
+  return new Intl.DisplayNames([language], {type: 'language'}).of(language);
+}
