2ac3e325414d91600da3265da7482bfc63d05660
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-02-18 18:29:00 +0100

    update page config

--- Database/EntityConfigurations/PageConfiguration.cs
+++ Database/EntityConfigurations/PageConfiguration.cs
@@ -1,7 +1,4 @@
-﻿using Domain.Category;
-using Domain.Events;
-using Domain.Locations;
-using Domain.Pages;
+﻿using Domain.Pages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -16,7 +13,17 @@ public class PageConfiguration : IEntityTypeConfiguration<Page>
     builder.HasKey(e => e.Id);
     builder.Property(e => e.Id).ValueGeneratedNever();
 
-    builder.Property(e => e.Title);
-    builder.Property(e => e.Description);
+    // builder.Property(e => e.Title);
+    // builder.Property(e => e.Description);
+
+    // Configure the Translations property to be stored as jsonb.
+    builder.Property(p => p.Translations)
+      // .HasConversion(
+      //   // Convert dictionary to JSON string for storage.
+      //   v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),
+      //   v => JsonSerializer
+      //          .Deserialize<Dictionary<string, PageI18NData>>(v, new JsonSerializerOptions())
+      //           ?? new Dictionary<string, PageI18NData>())
+      .HasColumnType("jsonb");
   }
 }
