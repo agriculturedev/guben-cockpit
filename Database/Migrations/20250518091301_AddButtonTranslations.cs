0b1e7668f268c62f6c25b5e9aa8aeaf14042a18e
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2025-05-18 15:02:43 +0200

    hotfix translation migrations (#106)

--- Database/Migrations/20250518091301_AddButtonTranslations.cs
+++ Database/Migrations/20250518091301_AddButtonTranslations.cs
@@ -10,6 +10,24 @@ namespace Database.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+          migrationBuilder.AddColumn<string>(
+            name: "Button_Translations",
+            schema: "Guben",
+            table: "InformationCard",
+            type: "jsonb",
+            nullable: true);
+
+          migrationBuilder.Sql(@"
+            UPDATE ""Guben"".""InformationCard""
+            SET ""Button_Translations"" = jsonb_build_object(
+              'de',
+              jsonb_build_object(
+                'Title', ""Button_Title"",
+                'Url', ""Button_Url""
+              )
+            )
+          ");
+
             migrationBuilder.DropColumn(
                 name: "Button_Title",
                 schema: "Guben",
@@ -20,12 +38,7 @@ namespace Database.Migrations
                 schema: "Guben",
                 table: "InformationCard");
 
-            migrationBuilder.AddColumn<string>(
-                name: "Button_Translations",
-                schema: "Guben",
-                table: "InformationCard",
-                type: "jsonb",
-                nullable: true);
+
         }
 
         /// <inheritdoc />
