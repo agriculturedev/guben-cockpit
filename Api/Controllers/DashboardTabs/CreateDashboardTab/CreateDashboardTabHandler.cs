63439fe52a9362766843ffa3d598405e50d2cf04
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2025-05-18 10:48:45 +0200

    add translations for dashboard tabs (#101)

--- Api/Controllers/DashboardTabs/CreateDashboardTab/CreateDashboardTabHandler.cs
+++ Api/Controllers/DashboardTabs/CreateDashboardTab/CreateDashboardTabHandler.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Api.Infrastructure.Extensions;
 using Domain.DashboardTab;
 using Domain.DashboardTab.repository;
@@ -8,17 +9,20 @@ namespace Api.Controllers.DashboardTabs.CreateDashboardTab;
 public class CreateDashboardTabHandler : ApiRequestHandler<CreateDashboardTabQuery, CreateDashboardTabResponse>
 {
   private readonly IDashboardRepository _dashboardRepository;
+  private readonly CultureInfo _culture;
 
   public CreateDashboardTabHandler(IDashboardRepository dashboardRepository)
   {
     _dashboardRepository = dashboardRepository;
+    _culture = CultureInfo.CurrentCulture;
+
   }
 
   public override async Task<CreateDashboardTabResponse> Handle(CreateDashboardTabQuery request, CancellationToken cancellationToken)
   {
     int nextSequence = _dashboardRepository.GetNextSequence();
 
-    var (result, dashboardTab) = DashboardTab.Create(request.Title, nextSequence, request.MapUrl, new List<InformationCard>());
+    var (result, dashboardTab) = DashboardTab.Create(request.Title, nextSequence, request.MapUrl, new List<InformationCard>(), _culture);
     result.ThrowIfFailure();
 
     await _dashboardRepository.SaveAsync(dashboardTab);
